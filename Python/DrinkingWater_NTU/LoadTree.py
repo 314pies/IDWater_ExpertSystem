92ed1b0086f18d6d0d279218d74a9e4186413d93
314pies <ian121363@gmail.com>
2017-04-30 15:57:45 +0800

    Add few line of test code

--- Python/DrinkingWater_NTU/LoadTree.py
+++ Python/DrinkingWater_NTU/LoadTree.py
@@ -3,11 +3,12 @@ import sys
 
 try :
     print('Importing DecisionTreeClassifier')
-    with open('ProtoType.pkl', 'rb') as fid:
+    with open('DrinkgingWater_NTU.pkl', 'rb') as fid:
         clf_gini = _pickle.load(fid)
 except:
     print("Failed to import DecisionTreeClassifier")
     sys.exit()
 
 #Predict dataset
-print(clf_gini.predict([[3,100,0]]))
+print(clf_gini.predict([[2.5,103,0]]))
+print(clf_gini.predict([[2.5,50,0]]))
